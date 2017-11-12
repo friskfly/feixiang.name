26f2a0a3c89a4b7c01a801133d33f1b64b34ccee
friskfly <friskfly@gmail.com>
2017-11-12 10:47:58 +0800

    update 2017-11-12 10:47:55

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
-hugo && rsync -Pavz --delete public/ fx@35.185.154.193:/usr/local/nginx/html
+# hugo && rsync -Pavz --delete public/ fx@35.185.154.193:/usr/local/nginx/html
+mkdir -p deployed && hugo && hugodeploy push -d
